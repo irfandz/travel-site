326c50a76e6bd4ccf3c5a9598e1ff1500c87ff56
Ulwan Irfandi <irfandz.oel1@gmail.com>
2022-10-09 15:00:06 +0700

    finishing modal functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,13 +2,15 @@ import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import Sticky from './modules/Sticky';
+import Modal from './modules/Modal';
 
-if (module.hot){
-    module.hot.accept();
-}
-
+new Modal();
 let stickyHeader = new Sticky();
 let mobileMenu = new MobileMenu();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 60); 
 new RevealOnScroll(document.querySelectorAll(".testimonials"), 60); 
+
+if (module.hot){
+    module.hot.accept();
+}
 
